1ab3472d9e48cffd864fda11bbb5804f01d1159e
Ngan Nguyen <ngannguyen977@gmail.com>
2017-07-20 15:32:57 +0700

    207

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
-import {RouterModule} from '@angular/router';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { NotificationService } from '../core/services/notification.service';
+import { AuthenService } from '../core/services/authen.service';
+import { MessageConstants } from '../core/common/message.constants';
+import { UrlConstants } from '../core/common/url.constants';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  model: any = {};
+  loading = false;
+  returnUrl: string;
+  constructor(
+    private authenService: AuthenService,
+    private notificationService: NotificationService,
+    private router: Router,
+  ) { }
 
   ngOnInit() {
   }
-
+  login() {
+    this.loading = true;
+    this.authenService.login(this.model.username, this.model.password).subscribe(data => {
+      this.router.navigate([UrlConstants.LOGIN])
+    }, error => {
+      this.notificationService.printErrorMessage(MessageConstants.SYSTEM_ERROR_MSG);
+      this.loading = false;
+    })
+  }
 }
